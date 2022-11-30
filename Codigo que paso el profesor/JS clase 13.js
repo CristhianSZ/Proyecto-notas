41f9ed3c73c2b56b58ec07a43de363a73c652b50
CristhianSZ <cristhiansosazurita@gmail.com>
2022-11-30 18:06:14 -0300

    Clase 14

---
+++ Codigo que paso el profesor/JS clase 13.js
@@ -0,0 +1,84 @@
+/*Ejercicio 1 - clase 11
+
+var sigla = null;
+
+function IngresarSigla(){
+    do{
+        sigla = prompt('Ingrese las siglas de su curso:')
+        sigla = sigla.toLocaleUpperCase();
+    }while(!(sigla == 'IP' || sigla == 'PDM'))
+
+}
+
+function MostrarCurso(){
+    if(sigla == null){
+        console.info('No hay siglas ingresadas')
+    }else{
+        if(sigla == 'IP'){
+            console.info('Cursa Introducción a la programación')
+        }else{
+            console.info('Cursa Programación de Dispositivos Móviles')
+        }
+    }
+
+}
+*/
+
+/*Ejercicio 2 - clase 11
+
+var num = null;
+
+function IngresarNumero(){
+    do{
+        num = parseInt(prompt('Ingresar un numero:'))
+    }while(isNaN(num))
+}
+
+function MostrarParidad(){
+    
+    if(num == null){
+        console.info('No hay un numero ingresado')
+    }else{
+        if(num == 0){
+            console.info('El numero ingresado es 0')
+        }else{
+            if(num % 2 == 0){
+                console.info('El numero ingresado es par')
+            }else{
+                console.info('El numero ingresado es impar')
+            }
+        }
+    }    
+}
+*/
+
+//EJERCICIO 1 - CLASE 13
+
+//var precio, impuesto;
+
+function Calcular(precio, impuesto = 21){
+    //precio = parseInt(prompt('Ingrese el precio: '))
+    //impuesto = parseInt(prompt('Ingrese el %: '))
+    //var precio = 100;
+    //var impuesto = 21;
+
+    if(isNaN(precio)){
+        //console.info('No ingresó un valor válido para el precio')
+        return 'No ingresó un valor válido para el precio';
+    }else{
+        if(isNaN(impuesto)){
+            //console.info('No ingresó un valor válido para el impuesto')
+            impuesto = 21;
+            console.warn('No ingresó un valor válido para el impuesto');
+        }else{
+            impuesto = impuesto >=0 && impuesto <=100 ? impuesto : 21;
+        }
+    
+        var iva = precio * impuesto / 100;
+        var total = precio + iva;
+    
+        //console.info(`Precio: $${precio} – IVA: ${iva}% – Total: $${total}`)
+        return `Precio: $${precio} – IVA: ${iva}% – Total: $${total}`;
+    }
+    
+}
---
+++ Ejercitacion/Ejercitacion funciones.js
@@ -0,0 +1,53 @@
+//*Una funcion resuelve una sola tarea
+//*A la funcion de le doy algo y me da un resultado 
+/**
+ * *Una funcion puede recibir un numero
+ * *La funcion trabaja
+ * *Me devuelve el resultado
+ * *Yo le doy un argumento al parametro de la funcion
+ * *Y me va a retornar (return)
+ * *Return es usado en funciones cuando esta devuelve un valor 
+ * *La funcion tiene que estar definida
+ * *La funcion esta compuesta por tres parte 
+ * *Tipos de funcion sugun los tipos de datos int entero float decimales char caracteres bool true o false y void especial para funciones
+ * *Una funicon tiene que tener un tipo 
+ * *Una funcion tiene que tener un nombre en general comienza con mayuscula
+ * *Parametros que va a recirbir
+ * *Los parametros se deviden en dos
+ * *Intn1,Intn2
+ * *Tengo que pone en claro que es lo que le voy a ingresar 
+ * *Voy a la funcion principal y llamo a la funcion y su resultado debo guardalo por asignacion 
+ * *El orden de las variables es clave
+ * * ¿QU es lo que sucede con los paramentro?
+ * !Parametros que pasan por valor y otro que pasa por referencia
+ * *Que quiere decir por valor :Creo la variable y adquiere un espacio el que le inicializo y se hace una copia en memoria
+ * *Nacen dos variable nuevas n1 y n2 con una copia del valor
+ * *Mientras viva la funcion yo tendre duplicados los valores
+ * *Es decir que si cambio el valor de la variable que esta inicializando valores desdes la variable desde fuera, al llamar la variable desde afuera esta tendra el valor que tiene afuera no el valor que modifique desde la funcion
+ * *CUando muere la funcion mueren sus variables y sus valores pero las de el exterior siguen intactas con su valor
+ * *Que es el scope?
+ * *Quien vive y existe en distintas dimensiones dentro y fuera de la funcion
+ * *Si las variable tienen el mismo nombre en ambos ambitos o scope la general tiene mas privilegio que la privada
+ * !Que paso cuando uso forma pasar valor por referencia
+ * *QUe es una funcion de tipo voil?
+ * *Es una funcion que no devuelve nada
+ * *Establezco una variable por fuera la cual tiene basura por ahora
+ * *EL procedimiento es el mismo hasta ahora se crean nuevas variables dentro de la memoria que reciben los valores que inicilizo desde fuera
+ * ?Establezco una variable dentro de la funcion que utiliza el mismo espacio de memoria que declaro por fuera de la funcion 
+ * *Desde afuera tiene un nombre y desde dentro otra y ocupan un mismo espacio
+ * *Desde afuera la variable nuestra el valor que se le dio desde dentro de la funcion
+ * *
+ *TODO Si a una variable general le asocio el valor de una variable privada obtengo algo similar a las variables publicas o la unica manera de acceder a las variables privadas es por el comando this?
+ */
+/**
+ * !Combinacion de funciones
+ * *SOlo cumple un sola funcion y estas interacturan entre ellas
+ * *Los bloques de codigos pueden ser reutilizables
+ * *globales 
+ * *Ejemplo----
+ */
+/**
+ * Funcion Tanto(     .    )
+ * var Variable = operacion
+ * retorno Variable
+ */
