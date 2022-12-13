fbddcd1b3618698e55cd827dd21955235fcf685b
CristhianSZ <cristhiansosazurita@gmail.com>
2022-12-13 18:17:32 -0300

    Clase 15

---
+++ Clase 15.js
@@ -0,0 +1,16 @@
+var aTablero = [];
+
+aTablero[0] = ["N","N","A","N","N","A","N","A"]
+aTablero[1] = ["A","A","A","A","A","A","N","A"]
+aTablero[2] = ["A","N","N","N","N","A","N","A"]
+aTablero[3] = ["A","A","A","A","A","A","N","A"]
+aTablero[4] = ["A","A","N","N","N","A","N","A"]
+aTablero[5] = ["A","A","A","A","A","A","A","A"]
+aTablero[6] = ["N","A","N","N","N","A","A","N"]
+aTablero[7] = ["N","A","A","A","A","A","A","N"]
+
+function MostrarTablero(){
+    console.info(aTablero)
+}
+
+MostrarTablero()
